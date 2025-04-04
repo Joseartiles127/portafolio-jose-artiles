06552185289d6974d905d75c595bc094db195d81
Joseartiles127 <richiezespiritusanto062@gmail.com>
2025-04-03 22:49:50 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("formularioContacto").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("¡Gracias por contactarme! Me comunicaré contigo pronto.");
+});
